a0c92bac5327796cd0a76995bac3942d0a34bc23
its-cleb <148484251+its-cleb@users.noreply.github.com>
2023-11-08 15:31:09 -0800

    Begin Flatlist

--- components/ActiveProjects.js
+++ components/ActiveProjects.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import { globalStyles } from '../styles/globalstyles'
 
 const ActiveProjects = () => {
+  const [activeProjects, setActiveProjects] = useState([])
   return (
     <View>
       <Text style={globalStyles.textTitle}>Active Projects</Text>
+      <FlatList>
+
+      </FlatList>
     </View>
   )
 }
