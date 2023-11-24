1626a3d44870e24cb3f2b8ce541fd5a4dd7023ce
its-cleb <148484251+its-cleb@users.noreply.github.com>
2023-11-24 13:10:32 -0800

    Add Project Modal added
    - Added TextButton component
    - Revised IconButtonHContent and TextButton components to accept onPress functions as props
    - Added modal functionality to ProjectScreen

---
+++ components/TextButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TouchableOpacity, Text } from 'react-native'
+import { globalStyles } from '../styles/globalstyles'
+
+const TextButton = props => {    
+
+  return (
+    <TouchableOpacity 
+      style={[globalStyles.touchableOpacityButton, { backgroundColor: `${props.bgcolor}`}]}
+      onPress={props.pressFunction}
+    > 
+      <Text style={globalStyles.textButton}>{props.text}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default TextButton
--- screens/ProjectsScreen.js
+++ screens/ProjectsScreen.js
@@ -1,32 +1,50 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Modal, Alert, Pressable } from 'react-native'
 import { SimpleLineIcons, MaterialIcons, MaterialCommunityIcons, FontAwesome, FontAwesome5 } from '@expo/vector-icons'
 import { globalStyles } from '../styles/globalstyles'
 import { useNavigation } from '@react-navigation/native'
 import IconButtonHContent from '../components/IconButtonHContent'
 import ActiveProjects from '../components/ActiveProjects'
+import TextButton from '../components/TextButton'
 
 
 const ProjectsScreen = () => {
+const [modalVisible, setModalVisible] = useState(false);
+
+const openModal = () => {
+  setModalVisible(true)
+}
+const closeModal = () => {
+  setModalVisible(false)
+}
+
   return (
     <>
     <View style={globalStyles.containerHCentered}>
       <Text style={globalStyles.textTitle}>Projects</Text>
     </View>
 
-    <ActiveProjects />
+    <IconButtonHContent pressFunction={openModal} title="Add Project" icon="plus" bgcolor="#00000000" textcolor="steelblue"/> 
+    
+    <Modal
+      animationType="slide"
+      visible={modalVisible}
+      presentationStyle="formSheet"
+      onRequestClose={() => {
+        setModalVisible(!modalVisible);
+      }}>
 
-    <TouchableOpacity onPress={() => test() }>
-      <IconButtonHContent title="Add Project" icon="plus" bgcolor="#00000000" textcolor="steelblue"/> 
-    </TouchableOpacity>
+      <View>
+        <Text> Test </Text>
+        <TextButton pressFunction={closeModal} bgcolor="steelblue" text="text"/>
+      </View>
+    </Modal>
     </>
   )
   
 }
 
-const test = () => {
-  console.log("test")
-}
 
 const styles = StyleSheet.create({})
 
